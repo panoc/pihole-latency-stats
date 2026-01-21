bb9396f7f3e9e55f8f42d3912c58b1ea6b88a5df
panoc <37024673+panoc@users.noreply.github.com>
2026-01-21 20:01:32 +0200

    Add files via upload

--- pihole_stats.sh
+++ pihole_stats.sh
@@ -22,7 +22,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ==============================================================================
 
-VERSION="3.3"
+VERSION="3.3.1"
 
 # --- 0. CRITICAL LOCALE FIX ---
 # Force C locale to prevent decimal errors (comma vs dot) in math operations
